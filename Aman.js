5aaa7d3a415e8f5ed97199b12c16dc3370798472
AmanKannaujiya <amankannaujiya799@gmail.com>
2025-11-20 14:06:56 +0530

    first commit

---
+++ Aman.js
@@ -0,0 +1,12 @@
+console.log("Helle Aman");
+console.log("Hello Aman How are you");
+let a = 10;
+let b = 20;
+if(a>b)
+{
+    console.log("A is greater than B");
+}
+else
+{
+    console.log("B is greater than A");
+}
